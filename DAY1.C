a53adef34acbd1e96034100c9ba9d01b045155f9
AmishaaPaliwal <muktapaliwal11@gmail.com>
2024-12-15 00:17:33 +0530

    Update DAY1.C

--- DAY1.C
+++ DAY1.C
@@ -1,1 +1,24 @@
 #include<stdio.h>
+
+int max(int arr){
+     int maxel = 0 ;
+    for(int i=0 ;i<5;i++){
+       
+        if(arr[maxel] <arr[i])
+        {maxi = i ;
+    }   
+        }
+        return maxel ;
+            }
+
+int main(){
+    int i ,arr[5] ;
+    for( i=0 ; i<5 ;i++){
+        printf("enter array elements at &d:",i);
+        scanf("%d",&arr[i]) ;
+    }
+
+    int maxele = max(arr) ;
+    printf("max elemnt in array is %d", arr[maxele]) ;
+    return 0 ;
+}
